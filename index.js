c32af10ed20cde1f49a28ff0f65d7152ace01936
Rahim-sarker <mhrahim08@gmail.com>
2022-12-03 13:22:54 +0600

    Complete Serversite

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const app = express()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT ||5000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello Rahim Sarker your Server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Rahim Sarker your Server is running");
+});
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnwxwtz.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
+const uri = `mongodb+srv://rahim:MjAVGqJ0DBjp4Atp@cluster0.dnwxwtz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
@@ -24,54 +25,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   client.close();
 // });
 
-
-
 async function run() {
   try {
     await client.connect();
-    
+
     const serviceCollection = client.db("geniuscar").collection("services");
 
-    app.get('/service', async(req,res)=>{
-       const query = {}
-       const cursor = serviceCollection.find(query);
-       const result = await cursor.toArray();
-       res.send(result);
-    })
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/service/:id', async(req,res)=>{
+    app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:ObjectId(id)};
-      const service =  await serviceCollection.findOne(query);
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
       res.send(service);
-    })
-
+    });
 
     //POST API
-    app.post('/service', async(req,res)=>{
-       const newService = req.body;
-       const result = await serviceCollection.insertOne(newService);
-       res.send(result);
-    })
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
 
     //Delete api
-    app.delete('/service/:id', async(req,res)=>{
-       const id = req.params.id;
-       const query = {_id: ObjectId(id)}
-       const result = await serviceCollection.deleteOne(query);
-       res.send(result);
-    })
-
-    
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
-   
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
